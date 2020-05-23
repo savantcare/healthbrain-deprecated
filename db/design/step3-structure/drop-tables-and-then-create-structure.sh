d3bccb25ac320058c6d2b04dac28c26aa144d6bf
Vikas <kedia.vikas@gmail.com>
2020-05-23 12:24:47 -0700

    bc

--- db/design/step3-structure/drop-tables-and-then-create-structure.sh
+++ db/design/step3-structure/drop-tables-and-then-create-structure.sh
@@ -9,10 +9,7 @@ do
 done
 
 
-# To make the above recurse into subdirectories (in bash), you can use the globstar option; also set dotglob to match files whose name begins with . Ref: https://unix.stackexchange.com/questions/139363/recursively-iterate-through-files-in-a-directory
-shopt -s globstar dotglob
-
-for file in **/*.sql
+for file in $(find . -mindepth 3 -type f -name "*.sql" -print)
 do
     echo "Executing the file $file"
     mysql -u stanford2008 --password=jaidurgama < $file 
