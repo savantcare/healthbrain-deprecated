a3cd545479a1821986ba3b8f0a26d6cad0072a71
Vikas <kedia.vikas@gmail.com>
2020-05-23 09:40:11 -0700

    bc

--- db/design/step3-structure/drop-tables-and-then-create-structure.sh
+++ db/design/step3-structure/drop-tables-and-then-create-structure.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Create the DB if not exist
+# Ref: https://stackoverflow.com/questions/51654041/loop-through-all-files-in-a-directory-and-subdirectories-using-bash
+for i in $(find . -type d -print)
+do
+    mysql -u stanford2008 --password=jaidurgama create database $i
+done
+
+
 # To make the above recurse into subdirectories (in bash), you can use the globstar option; also set dotglob to match files whose name begins with . Ref: https://unix.stackexchange.com/questions/139363/recursively-iterate-through-files-in-a-directory
 shopt -s globstar dotglob
 
