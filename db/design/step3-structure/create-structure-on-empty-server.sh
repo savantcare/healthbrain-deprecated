a74d2c9420a9d4a0e53d6dddf34b8b2821970d87
Vikas <kedia.vikas@gmail.com>
2020-05-23 00:31:07 -0700

    adding the dump date

--- db/design/step3-structure/create-structure-on-empty-server.sh
+++ db/design/step3-structure/create-structure-on-empty-server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# To make the above recurse into subdirectories (in bash), you can use the globstar option; also set dotglob to match files whose name begins with . Ref: https://unix.stackexchange.com/questions/139363/recursively-iterate-through-files-in-a-directory
+shopt -s globstar dotglob
+
 for file in **/*.sql
 do
     echo "Executing the file $file"
